c2291cbdbf049315571438c6392b271be6de0670
telome <>
2023-05-31 15:27:31 +0100

    Account for ES

--- src/Swapper.sol
+++ src/Swapper.sol
@@ -11,6 +11,7 @@ interface BoxLike { // aka "Conduit"
 }
 
 interface GemLike {
+    function balanceOf(address) external view returns (uint256);
     function approve(address, uint256) external returns (bool);
     function transfer(address, uint256) external returns (bool);
     function transferFrom(address, address, uint256) external returns (bool);
@@ -18,6 +19,10 @@ interface GemLike {
 
 interface PipLike {
     function read() external view returns (bytes32);
+}
+
+interface VatLike {
+    function live() external view returns (uint256);
 }
 
 // https://github.com/Uniswap/v3-periphery/blob/b06959dd01f5999aa93e1dc530fe573c7bb295f6/contracts/SwapRlefter.sol
@@ -43,6 +48,7 @@ contract Swapper {
     mapping (address => uint256) public keepers;  // whitelisted keepers
     mapping (address => uint256) public boxes;    // whitelisted conduits
     
+    VatLike public vat;
     address public buffer;     // Allocation buffer for this Swapper
     PipLike public pip;        // Reference price oracle in DAI/USDC
     uint256 public hop;        // [seconds] swap cooldown
@@ -50,8 +56,6 @@ contract Swapper {
     uint256 public maxOut;     // [WAD] max amount swapped from gem to dai every hop
     uint256 public zzz;        // [Timestamp]   Last swap
     uint256 public fee;        // [BPS] UniV3 pool fee
-    bytes   public pathIn;     // [ABI-encoded] UniV3 compatible path
-    bytes   public pathOut;    // [ABI-encoded] UniV3 compatible path
     uint256 public want;       // [WAD]         Relative multiplier of the reference price to insist on in the swap.
 
     event Rely  (address indexed usr);
@@ -64,16 +68,18 @@ contract Swapper {
     event File  (bytes32 indexed what, address data);
     event File  (bytes32 indexed what, address data, uint256 val);
     event Swap  (address indexed keeper, address indexed from, address indexed to, uint256 wad, uint256 out);
+    event Quit  (address indexed usr, uint256 wad);
 
     struct Load {
         address box;
         uint256 wad; // [WAD]
     }
 
-    constructor(address _dai, address _gem, address _uniV3Router) {
-        uniV3Router = _uniV3Router;
+    constructor(address _vat, address _dai, address _gem, address _uniV3Router) {
+        vat = VatLike(_vat);
         dai = _dai;
         gem = _gem;
+        uniV3Router = _uniV3Router;
 
         GemLike(dai).approve(uniV3Router, type(uint256).max);
         GemLike(gem).approve(uniV3Router, type(uint256).max);
@@ -134,7 +140,7 @@ contract Swapper {
     }
 
     function swapIn(uint256 wad) external keeper returns (uint256 out) {
-
+        require(vat.live() == 1, "Swapper/vat-not-live");
         require(block.timestamp >= zzz + hop, "Swapper/too-soon");
         zzz = block.timestamp;
 
@@ -158,6 +164,7 @@ contract Swapper {
     }
 
     function push(Load[] calldata to) external toll {
+        require(vat.live() == 1, "Swapper/vat-not-live");
         for(uint256 i; i < to.length;) {
             (address _to, uint256 _wad) = (to[i].box,  to[i].wad);
             require(boxes[_to] == 1, "Swapper/invalid-destination");
@@ -167,24 +174,28 @@ contract Swapper {
         }
     }
 
-    function pull(Load[] calldata from) external toll {
-        // Q: can we assume box.completeWithdraw(withdrawId) has already been performed for all from[i].box ?
+    function pull(Load[] calldata from) external {
+        // After ES, we want to allow permissionlessly bring all the gem back
+        require(buds[msg.sender] == 1 || vat.live() == 0, "Swapper/non-keeper"); 
 
         for(uint256 i; i < from.length;) {
             (address _from, uint256 _wad) = (from[i].box,  from[i].wad);
             // require(boxes[_from] == 1, "Swapper/invalid-source");
 
-            // Q: swappers could call an auth method of the conduit but that would probably not be scalable or a good separation between subdao, 
-            // so instead we could maybe specify operator address when doing initiateWithdraw() and have the conduit approve(operator, wad) during completeWithdrawal()
+            // We assume the swapper was set as operator when calling box.initiateWithdrawal() and has
+            // subsequently been granted a gem allowance by the box in box.completeWithdrawal()
             require(GemLike(gem).transferFrom(_from, address(this), _wad));
             unchecked { ++i; }
         }
     }
 
-    function swapOut(uint256 wad) external keeper returns (uint256 out) {
-
-        require(block.timestamp >= zzz + hop, "Swapper/too-soon");
-        zzz = block.timestamp;
+    function swapOut(uint256 wad) external returns (uint256 out) {
+        // After ES, we want to allow permissionlessly swap all the gem back to dai and send that dai back to the buffer
+        if(vat.live() == 1) {
+            require(keepers[msg.sender] == 1, "Swapper/non-keeper"); 
+            require(block.timestamp >= zzz + hop, "Swapper/too-soon");
+            zzz = block.timestamp;
+        }
 
         require(wad <= maxOut, "Swapper/exceeds-max-out");
         
@@ -205,4 +216,12 @@ contract Swapper {
 
         emit Swap(msg.sender, gem, dai, wad, out);
     }
+
+    // After ES, we want to allow permissionlessly bring all the dai back to the buffer
+    function quit() external {
+        require(vat.live() == 0, "Swapper/vat-still-live");
+        uint256 wad = GemLike(dai).balanceOf(address(this));
+        GemLike(dai).transfer(buffer, wad);
+        emit Quit(msg.sender, wad);
+    } 
 }
