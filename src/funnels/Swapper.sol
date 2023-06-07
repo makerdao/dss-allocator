dec8f68abe6b1b1034168320260d715c9bc73e7c
telome <>
2023-06-07 15:58:10 +0100

    Add updated Swapper

--- src/funnels/Swapper.sol
+++ src/funnels/Swapper.sol
@@ -21,26 +21,16 @@ interface BufferLike {
     function take(address to, uint256 wad) external;
 }
 
-interface BoxLike { // aka "Conduit"
-    function deposit(address gem, uint256 wad) external;
-}
-
 interface GemLike {
     function decimals() external view returns (uint8);
-    function balanceOf(address) external view returns (uint256);
     function approve(address, uint256) external returns (bool);
     function transfer(address, uint256) external returns (bool);
     function transferFrom(address, address, uint256) external returns (bool);
 }
 
-interface VatLike {
-    function live() external view returns (uint256);
-}
-
 // https://github.com/Uniswap/v3-periphery/blob/b06959dd01f5999aa93e1dc530fe573c7bb295f6/contracts/SwapRlefter.sol
 interface SwapRouterLike {
     function exactInput(ExactInputParams calldata params) external returns (uint256 amountOut);
-    function factory() external returns (address factory);
 
     // https://github.com/Uniswap/v3-periphery/blob/b06959dd01f5999aa93e1dc530fe573c7bb295f6/contracts/interfaces/ISwapRouter.sol#L26
     // https://docs.uniswap.org/protocol/guides/swaps/multihop-swaps#input-parameters
@@ -57,26 +47,19 @@ contract Swapper {
     mapping (address => uint256) public wards;
     mapping (address => uint256) public buds;     // whitelisted facilitators
     mapping (address => uint256) public keepers;  // whitelisted keepers
-    mapping (address => uint256) public boxes;    // whitelisted conduits
-    
+
     address public buffer;         // Allocation buffer for this Swapper
+    address public escrow;         // Contract from which the GEM to sell is pulled before a GEM-to-NST swap or to which the bought GEM is pushed after a NST-to-GEM swap
     uint256 public hop;            // [seconds]   Swap cooldown (set by governance)
-    uint256 public nstToGemCount;  // [WAD]       Remaining number of time that a nst-to-gem swap can be performed (set by facilitators)
-    uint256 public gemToNstCount;  // [WAD]       Remaining number of time that a gem-to-nst swap can be performed (set by facilitators)
+    uint256 public nstToGemCount;  // [count]     Remaining number of times that a nst-to-gem swap can be performed (set by facilitators)
+    uint256 public gemToNstCount;  // [count]     Remaining number of times that a gem-to-nst swap can be performed (set by facilitators)
     uint256 public nstLot;         // [WAD]       Amount swapped from nst to gem every hop (set by facilitators)
     uint256 public gemLot;         // [WAD]       Amount swapped from gem to nst every hop (set by facilitators)
     uint256 public maxNstLot;      // [WAD]       Max allowable nstLot (set by governance)
     uint256 public maxGemLot;      // [WAD]       Max allowable gemLot (set by governance)
-    uint256 public gemWant;        // [WAD]       Relative multiplier of the reference price (equal to 1 gem/nst) to insist on in the swap from nst to gem (set by governance)
-    uint256 public nstWant;        // [WAD]       Relative multiplier of the reference price (equal to 1 nst/gem) to insist on in the swap from gem to nst (set by governance)
+    uint256 public minNstPrice;    // [WAD]       Relative multiplier of the reference price (equal to 1 gem/nst) to insist on in the swap from nst to gem (set by governance)
+    uint256 public minGemPrice;    // [WAD]       Relative multiplier of the reference price (equal to 1 nst/gem) to insist on in the swap from gem to nst (set by governance)
     uint256 public zzz;            // [Timestamp] Last swap
-
-    uint256[] internal weights;   // Bit vector tightly packing (address(box), uint96(percentage)) tuple entries such that the sum of all percentages = 1 WAD (100%) (set by facilitators)
-
-    struct Weight {
-        address box;
-        uint96 wad; // percentage in WAD such that 1 WAD = 100%
-    }
 
     event Rely   (address indexed usr);
     event Deny   (address indexed usr);
@@ -89,7 +72,6 @@ contract Swapper {
     event File   (bytes32 indexed what, address data, uint256 val);
     event Lots   (address indexed bud, uint256 nstLot, uint256 gemLot);
     event Counts (address indexed bud, uint256 nstToGemCount, uint256 gemToNstCount);
-    event Weights(address indexed bud, Weight[] weights);
     event Swap   (address indexed kpr, address indexed from, address indexed to, uint256 amt, uint256 out);
     event Quit   (address indexed usr, uint256 wad);
 
@@ -141,25 +123,20 @@ contract Swapper {
     function forbid(address usr) external toll { keepers[usr] = 0; emit Forbid(usr); }
 
     function file(bytes32 what, uint256 data) external auth {
-        if      (what == "maxNstLot")  maxNstLot = data;
-        else if (what == "maxGemLot")  maxGemLot = data;
-        else if (what == "gemWant")    gemWant   = data;
-        else if (what == "nstWant")    nstWant   = data;
-        else if (what == "hop")        hop       = data;
+        if      (what == "maxNstLot")   maxNstLot   = data;
+        else if (what == "maxGemLot")   maxGemLot   = data;
+        else if (what == "minNstPrice") minNstPrice = data;
+        else if (what == "minGemPrice") minGemPrice = data;
+        else if (what == "hop")         hop         = data;
         else revert("Swapper/file-unrecognized-param");
         emit File(what, data);
     }
 
     function file(bytes32 what, address data) external auth {
-        if (what == "buffer") buffer = data;
+        if      (what == "escrow") escrow = data;
+        else if (what == "buffer") buffer = data;
         else revert("Swapper/file-unrecognized-param");
         emit File(what, data);
-    }
-
-    function file(bytes32 what, address data, uint256 val) external auth {
-        if (what == "box") boxes[data] = val;
-        else revert("Swapper/file-unrecognized-param");
-        emit File(what, data, val);
     }
 
     function setLots(uint256 _nstLot, uint256 _gemLot)  external toll {
@@ -176,36 +153,12 @@ contract Swapper {
         emit Counts(msg.sender, _nstToGemCount, _gemToNstCount);
     }
 
-    function setWeights(Weight[] memory newWeights) external toll {
-        uint256 cumPct;
-        uint256[] memory arr = new uint256[](newWeights.length);
-        for(uint256 i; i < newWeights.length;) {
-            (address _box, uint256 _pct) = (newWeights[i].box, uint256(newWeights[i].wad));
-            arr[i] = (uint256(uint160(_box)) << 96) | _pct;
-            cumPct += _pct;
-            unchecked { ++i; }
-        }
-        require(cumPct == WAD, "Swapper/total-weight-not-wad");
-        weights = arr;
-        emit Weights(msg.sender, newWeights);
-    }
-
-    function getWeightAt(uint256 i) public view returns (address box, uint256 percent) {
-        uint256 weight = weights[i];
-        (box, percent) = (address(uint160(weight >> 96)), weight & (2**96 - 1));
-        require(boxes[box] == 1, "Swapper/invalid-box"); // sanity check that any deauthorised box has also been removed from the weights vector
-    }
-
-    function getWeightsLength() external view returns (uint256 len) {
-        len = weights.length;
-    }
-
     function nstToGem(uint256 min) external keeper returns (uint256 out) {
         require(block.timestamp >= zzz + hop, "Swapper/too-soon");
         zzz = block.timestamp;
 
         uint256 amt = nstLot;
-        require(min >= amt * gemWant / 10**(36 - decimals), "Swapper/min-too-small"); // 1/10^(36-d) = 1/WAD * 1/WAD * 10^d
+        require(min >= amt * minNstPrice / 10**(36 - decimals), "Swapper/min-too-small"); // 1/10^(36-d) = 1/WAD * 1/WAD * 10^d
 
         uint256 cnt = nstToGemCount;
         require(cnt > 0, "Swapper/exceeds-count");
@@ -223,17 +176,7 @@ contract Swapper {
         });
         out = SwapRouterLike(uniV3Router).exactInput(params);
 
-        uint256 pending = out;
-        uint256 len = weights.length;
-        for(uint256 i; i < len;) {
-            (address _to, uint256 _percent) = getWeightAt(i);
-            uint256 _amt = (i == len - 1) ? pending : out * _percent / WAD;
-
-            require(GemLike(gem).transfer(_to, _amt));
-
-            pending -= _amt;
-            unchecked { ++i; }
-        }
+        GemLike(gem).transfer(escrow, out);
 
         emit Swap(msg.sender, nst, gem, amt, out);
     }
@@ -243,25 +186,13 @@ contract Swapper {
         zzz = block.timestamp;
 
         uint256 amt = gemLot;
-        require(min >= amt * nstWant / 10**decimals, "Swapper/min-too-small"); // 1/10^d = 1/10^d * 1/WAD * WAD
+        require(min >= amt * minGemPrice / 10**decimals, "Swapper/min-too-small"); // 1/10^d = 1/10^d * 1/WAD * WAD
         
         uint256 cnt = gemToNstCount;
         require(cnt > 0, "Swapper/exceeds-count");
         gemToNstCount = cnt - 1;
 
-        uint256 pending = amt;
-        uint256 len = weights.length;
-        for(uint256 i; i < len;) {
-            (address _from, uint256 _percent) = getWeightAt(i);
-            uint256 _amt = (i == len - 1) ? pending : amt * _percent / WAD;
-
-            // We assume the swapper was set as operator when calling box.initiateWithdrawal() and has
-            // subsequently been granted a gem allowance by the box in box.completeWithdrawal()
-            require(GemLike(gem).transferFrom(_from, address(this), _amt));
-
-            pending -= _amt;
-            unchecked { ++i; }
-        }
+        GemLike(gem).transferFrom(escrow, address(this), amt);
 
         bytes memory path = abi.encodePacked(gem, uint24(fee), nst);
         SwapRouterLike.ExactInputParams memory params = SwapRouterLike.ExactInputParams({
--- src/test/funnels/Swapper.t.sol
+++ src/test/funnels/Swapper.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.16;
 
 import "../../funnels/Swapper.sol";
+import "../../funnels/Escrow.sol";
 import "dss-test/DssTest.sol";
 
 contract BufferMock {
@@ -14,75 +15,41 @@ contract BufferMock {
     }
 }
 
-contract BoxMock {
-    uint256 nextWithdrawId;
-    struct Withdrawal {
-        address token;
-        address operator;
-        uint256 amount;
-    }
-    mapping(uint256 => Withdrawal) withdrawals;
-    function initiateWithdraw(address token, address operator, uint256 amount) external returns (uint256) {
-        withdrawals[nextWithdrawId].token = token;
-        withdrawals[nextWithdrawId].operator = operator;
-        withdrawals[nextWithdrawId].amount = amount;
-        return nextWithdrawId++;
-    }
-    function completeWithdraw(uint256 withdrawId) external {
-        GemLike(withdrawals[withdrawId].token).approve(withdrawals[withdrawId].operator, withdrawals[withdrawId].amount);
-        delete withdrawals[withdrawId];
-    }
-}
 
 contract SwapperTest is DssTest {
     Swapper public swapper;
-    BoxMock public box1;
-    BoxMock public box2;
 
     address constant DAI          = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address constant USDC         = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address constant UNIV3_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
 
-    address constant FACILITATOR = address(0xb0b);
-    address constant KEEPER      = address(0x1337);
+    address constant FACILITATOR = address(0x1337);
+    address constant KEEPER      = address(0xb0b);
 
     function setUp() public {
         swapper = new Swapper(DAI, USDC, UNIV3_ROUTER, 100);
-        box1 = new BoxMock();
-        box2 = new BoxMock();
+        address escrow = address(new Escrow());
+        Escrow(escrow).approve(USDC, address(swapper), type(uint256).max);
         address buffer = address(new BufferMock(DAI));
         deal(DAI, buffer, 1_000_000_000 ether, true);
         uint256 maxNstLot = 10_000 ether;
         uint256 maxGemLot = 11_000 ether / 10**12;
-        swapper.file("box", address(box1), 1);
-        swapper.file("box", address(box2), 1);
+        swapper.file("escrow", escrow);
         swapper.file("buffer", buffer);
         swapper.file("maxNstLot", maxNstLot);
         swapper.file("maxGemLot", maxGemLot);
-        swapper.file("gemWant", 99 ether / 100);
-        swapper.file("nstWant", 99 ether / 100);
+        swapper.file("minNstPrice", 99 ether / 100);
+        swapper.file("minGemPrice", 99 ether / 100);
         swapper.kiss(FACILITATOR);
         vm.prank(FACILITATOR); swapper.setLots(maxNstLot, maxGemLot);
         vm.prank(FACILITATOR); swapper.setCounts(1, 1);
         vm.prank(FACILITATOR); swapper.permit(KEEPER);
-
-        Swapper.Weight[] memory weights = new Swapper.Weight[](2);
-        weights[0].box = address(box1);
-        weights[0].wad = 0.2 ether; // 20%
-        weights[1].box = address(box1);
-        weights[1].wad = 0.8 ether; // 20%
-        vm.prank(FACILITATOR); swapper.setWeights(weights);
     }
 
     function testSwap() public {
         uint256 nstLot = swapper.nstLot();
         vm.prank(KEEPER); uint256 gemOut = swapper.nstToGem(nstLot * 995/1000 / 10**12);
         console2.log("gemOut:", gemOut * 10**12);
-
-        uint256 withdrawId1 = box1.initiateWithdraw(USDC, address(swapper), gemOut);
-        uint256 withdrawId2 = box2.initiateWithdraw(USDC, address(swapper), gemOut);
-        box1.completeWithdraw(withdrawId1);
-        box2.completeWithdraw(withdrawId2);
 
         vm.prank(FACILITATOR); swapper.setLots(nstLot, gemOut);
         vm.prank(KEEPER); uint256 daiOut = swapper.gemToNst(gemOut * 995/1000 * 10**12);
