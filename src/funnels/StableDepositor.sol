090e2815c229278894a02a29b6a7f43f94a4e8cc
oldchili <130549691+oldchili@users.noreply.github.com>
2023-06-28 21:59:01 +0300

    tmp - wip

--- src/funnels/StableDepositor.sol
+++ src/funnels/StableDepositor.sol
@@ -67,19 +67,13 @@ interface UniV3PoolLike {
 
 // Mock for now
 library LiquidityAmounts {
-    function getLiquidityForAmount0(
+    function getLiquidityForAmounts(
         uint160,
         uint160,
+        uint160,
+        uint256,
         uint256
-    ) internal pure returns (uint128) {
-        return uint128(0);
-    }
-
-    function getLiquidityForAmount1(
-        uint160,
-        uint160,
-        uint256
-    ) internal pure returns (uint128) {
+    ) internal pure returns (uint128 liquidity) {
         return uint128(0);
     }
 }
@@ -94,31 +88,40 @@ library TickMath {
 contract StableDepositor {
     mapping (address => uint256) public wards;
     mapping (address => uint256) public buds;
-    
+
     DepositorLike public depositor;
 
-    mapping (address => mapping (address => uint256)) public lots;
-    mapping (address => mapping (address => uint256)) public counts;
-    mapping (address => mapping (address => uint24))  public fees;
-
-    mapping (address => mapping (address => int24)) public tickLowers;
-    mapping (address => mapping (address => int24)) public tickUppers;
-
-    mapping (address => mapping (address => int24)) public reqMinTicks; // Assumed within tickLower and tickUpper
-    mapping (address => mapping (address => int24)) public reqMaxTicks; // Assumed within tickLower and tickUpper
+    mapping (address => mapping (address => Pair))   public pairs;
+    mapping (address => mapping (address => Amount)) public amounts;
 
     bytes32 public immutable ilk;
     address internal immutable uniV3Factory; // 0x1F98431c8aD98523631AE4a59f267346ea31F984
+
+    struct Pair {
+        uint16 count;
+        uint24 fee;
+        int24  tickLower;
+        int24  tickUpper;
+    }
+
+    struct Amount {
+        uint128 amt0;
+        uint128 amt1;
+        uint128 minAmt0;
+        uint128 minAmt1;
+    }
 
     event Rely(address indexed usr);
     event Deny(address indexed usr);
     event Kissed(address indexed usr);
     event Dissed(address indexed usr);
     event File(bytes32 indexed what, address data);
-    event File(bytes32 indexed what, address indexed src, address indexed dst, uint256 data);
-    event File(bytes32 indexed what, address indexed src, address indexed dst, int24 data);
-    event Deposit(address indexed src, address indexed dst, uint256 liquidity, uint256 amtSrc, uint256 amtDst);
-    event Withdraw(address indexed src, address indexed dst, uint256 liquidity, uint256 amtSrc, uint256 amtDst);
+
+    event File(bytes32 indexed what, address indexed gem0, address indexed gem1, uint16 data);
+
+    event File(bytes32 indexed what, address indexed gem0, address indexed gem1, int24 data);
+    event Deposit(address indexed gem0, address indexed gem1, uint256 liquidity, uint256 amt0, uint256 amt1);
+    event Withdraw(address indexed gem0, address indexed gem1, uint256 liquidity, uint256 amt0, uint256 amt1);
 
     constructor(bytes32 ilk_, address uniV3Factory_) {
         ilk = ilk_;
@@ -141,22 +144,38 @@ contract StableDepositor {
         emit File(what, data);
     }
 
-    function file(bytes32 what, address src, address dst, uint256 data) external auth {
-        if      (what == "lot")   lots[src][dst]   = data;
-        else if (what == "count") counts[src][dst] = data;
-        else revert("StableDepositor/file-unrecognized-param");
-        emit File(what, src, dst, data);
+    function file(bytes32 what, address gem0, address gem1, uint16 data) external auth {
+        (gem0, gem1) = gem0 < gem1 ? (gem0, gem1) : (gem1, gem0);
+        if (what == "count") pairs[gem0][gem1].count = data;
+        else revert("Depositor/file-unrecognized-param");
+        emit File(what, gem0, gem1, data);
     }
 
-    function file(bytes32 what, address src, address dst, int24 data) external auth {
-        if      (what == "tickLower")  tickLowers[src][dst]  = int24(data);
-        else if (what == "tickUpper")  tickUppers[src][dst]  = int24(data);
-        else if (what == "reqMinTick") reqMinTicks[src][dst] = int24(data);
-        else if (what == "reqMaxTick") reqMaxTicks[src][dst] = int24(data);
-        else revert("StableDepositor/file-unrecognized-param");
-        emit File(what, src, dst, data);
+    function file(bytes32 what, address gem0, address gem1, uint16 data) external auth {
+        (gem0, gem1) = gem0 < gem1 ? (gem0, gem1) : (gem1, gem0);
+        if (what == "count") pairs[gem0][gem1].count = data;
+        else revert("Depositor/file-unrecognized-param");
+        emit File(what, gem0, gem1, data);
     }
 
+    /*
+    function file(bytes32 what, address gem0, address gem1, uint256 data) external auth {
+        (gem0, gem1) = gem0 < gem1 ? (gem0, gem1) : (gem1, gem0);
+        if      (what == "lot")   lots[gem0][gem1]   = data;
+        else if (what == "count") counts[gem0][gem1] = data; // TODO: add missing files
+        else revert("StableDepositor/file-unrecognized-param");
+        emit File(what, gem0, gem1, data);
+    }
+
+    function file(bytes32 what, address gem0, address gem1, int24 data) external auth {
+        (gem0, gem1) = gem0 < gem1 ? (gem0, gem1) : (gem1, gem0);
+        if      (what == "tickLower")  tickLowers[gem0][gem1]  = int24(data);
+        else if (what == "tickUpper")  tickUppers[gem0][gem1]  = int24(data);
+        else revert("StableDepositor/file-unrecognized-param");
+        emit File(what, gem0, gem1, data);
+    }
+*/
+    // TODO: can extract to library and share with other contracts
     // https://github.com/Uniswap/v3-periphery/blob/464a8a49611272f7349c970e0fadb7ec1d3c1086/contracts/libraries/PoolAddress.sol#L33
     function getPoolAddress(address gem0, address gem1, uint24 fee) internal view returns (address pool) {
         pool = address(uint160(uint256(keccak256(abi.encodePacked(
@@ -167,97 +186,76 @@ contract StableDepositor {
         )))));
     }
 
-    // TODO: do we want to let the keeper set minTick, maxTick like in the current swapper? (then we need to pass lot as well)
     struct StableDepositorParams {
-        address src;
-        address dst;
-        address swapperCallee;
-        bytes swapperData;
+        address gem0;
+        address gem1;
     }
 
     function deposit(StableDepositorParams memory p) toll external returns (uint128 liquidity, uint256 amt0, uint256 amt1) {
-        address _src = p.src;
-        address _dst = p.dst;
+        // Note: we rely on Depositor to enforce p.gem0 < p.gem1
+        address _gem0 = p.gem0;
+        address _gem1 = p.gem1;
 
-        uint256 _cnt = counts[_src][_dst];
-        require(_cnt > 0, "StableDepositor/exceeds-count");
-        counts[_src][_dst] = _cnt - 1;
+        Pair memory pair = pairs[_gem0][_gem1];
+        uint16 _count    = pair.count;
+        require(_count > 0, "StableDepositor/exceeds-count");
+        pair.count = _count - 1;
+        pairs[_gem0][_gem1] = pair;
 
-        (address _gem0, address _gem1) = (_src < _dst) ? (_src, _dst) : (_dst, _src);
-        uint24 _fees = fees[_src][_dst];
-        (, int24 _tick,,,,,) = UniV3PoolLike(getPoolAddress(_gem0, _gem1, _fees)).slot0();
-        require(_tick >= reqMinTicks[_src][_dst], "StableDepositor/price-below-min");
-        require(_tick <= reqMaxTicks[_src][_dst], "StableDepositor/price-above-max");
-
-        uint256 _lot = lots[_src][_dst];
+        Amount memory amount = amounts[_gem0][_gem1];
         DepositorLike.DepositParams memory depositParams = DepositorLike.DepositParams({
             gem0     : _gem0,
             gem1     : _gem1,
-            amt0     : (_src < _dst) ? _lot : type(uint256).max,
-            amt1     : (_src < _dst) ? type(uint256).max : _lot,
-            minAmt0  : 0,
-            minAmt1  : 0,
-            fee      : _fees,
-            tickLower: tickLowers[_src][_dst],
-            tickUpper: tickUppers[_src][_dst]
+            amt0     : amount.amt0,
+            amt1     : amount.amt1,
+            minAmt0  : amount.minAmt0,
+            minAmt1  : amount.minAmt1,
+            fee      : pair.fee,
+            tickLower: pair.tickLower,
+            tickUpper: pair.tickUpper
         });
         (liquidity, amt0, amt1) = depositor.deposit(depositParams);
 
-        emit Deposit(
-            _src,
-            _dst,
-            liquidity,
-            (_src < _dst) ? amt0 : amt1,
-            (_src < _dst) ? amt1 : amt0
-        );
+        emit Deposit(_gem0, _gem1, liquidity, amt0, amt1);
     }
 
     function withdraw(StableDepositorParams memory p) toll external returns (uint128 liquidity, uint256 amt0, uint256 amt1) {
-        address _src = p.src;
-        address _dst = p.dst;
+        // Note: we rely on Depositor to enforce p.gem0 < p.gem1
+        address _gem0 = p.gem0;
+        address _gem1 = p.gem1;
 
-        uint256 _cnt = counts[_src][_dst];
-        require(_cnt > 0, "StableDepositor/exceeds-count");
-        counts[_src][_dst] = _cnt - 1;
+        Pair memory pair = pairs[_gem0][_gem1];
+        uint16 _count    = pair.count;
+        require(_count > 0, "StableDepositor/exceeds-count");
+        pair.count = _count - 1;
+        pairs[_gem0][_gem1] = pair;
 
-        (address _gem0, address _gem1) = (_src < _dst) ? (_src, _dst) : (_dst, _src);
-        (uint160 _sqrtPriceX96, int24 _tick,,,,,) = UniV3PoolLike(getPoolAddress(_gem0, _gem1, fees[_src][_dst])).slot0();
-        require(_tick >= reqMinTicks[_src][_dst], "StableDepositor/price-below-min");
-        require(_tick <= reqMaxTicks[_src][_dst], "StableDepositor/price-above-max");
+        uint24 _fee = pair.fee;
+        (uint160 _sqrtPriceX96,,,,,,) = UniV3PoolLike(getPoolAddress(_gem0, _gem1, _fee)).slot0();
 
-        // TODO: check if this makes sense
-        uint256 _lot = lots[_src][_dst];
-        if (_src < _dst) {
-            liquidity = LiquidityAmounts.getLiquidityForAmount0(
-                TickMath.getSqrtRatioAtTick(tickLowers[_src][_dst]),
-                _sqrtPriceX96,
-                _lot);
-        } else {
-            liquidity = LiquidityAmounts.getLiquidityForAmount1(
-                _sqrtPriceX96,
-                TickMath.getSqrtRatioAtTick(tickUppers[_src][_dst]),
-                _lot
-            );
-        }
+        int24 _tickLower = pair.tickLower;
+        int24 _tickUpper = pair.tickUpper;
+        Amount memory amount = amounts[_gem0][_gem1];
+        liquidity = LiquidityAmounts.getLiquidityForAmounts(
+            _sqrtPriceX96,
+            TickMath.getSqrtRatioAtTick(_tickLower),
+            TickMath.getSqrtRatioAtTick(_tickUpper),
+            amount.amt0,
+            amount.amt1
+        );
 
         DepositorLike.WithdrawParams memory withdrawParams = DepositorLike.WithdrawParams({
             gem0     : _gem0,
             gem1     : _gem1,
             liquidity: liquidity,
-            minAmt0  : (_src < _dst) ? _lot : uint256(0),
-            minAmt1  : (_src < _dst) ? uint256(0) : _lot,
-            fee      : fees[_src][_dst],
-            tickLower: tickLowers[_src][_dst],
-            tickUpper: tickUppers[_src][_dst]
+            minAmt0  : amount.minAmt0,
+            minAmt1  : amount.minAmt1,
+            fee      : _fee,
+            tickLower: _tickLower,
+            tickUpper: _tickUpper
         });
         (amt0, amt1) = depositor.withdraw(withdrawParams);
 
-        emit Withdraw(
-            _src,
-            _dst,
-            liquidity,
-            (_src < _dst) ? amt0 : amt1,
-            (_src < _dst) ? amt1 : amt0
-        );
+        emit Withdraw(_gem0, _gem1, liquidity, amt0, amt1);
     }
 }
