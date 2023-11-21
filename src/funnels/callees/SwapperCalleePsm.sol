9318d514dfdd52c6181adbbd1ab9ed2139806737
telome <130504305+telome@users.noreply.github.com>
2023-11-21 14:14:15 +0000

    SwapperCalleePsm: Add note regarding dust accumulation (#72)
    * SwapperCalleePsm: Prevent swap amts not multiple of to18ConversionFactor
    
    * Remove requirement on SwapperCalleePsm amt
    
    ---------
    
    Co-authored-by: telome <>

--- src/funnels/callees/SwapperCalleePsm.sol
+++ src/funnels/callees/SwapperCalleePsm.sol
@@ -64,6 +64,8 @@ contract SwapperCalleePsm {
         emit Deny(usr);
     }
 
+    // Note: To avoid accumulating dust in this contract, `amt` should be a multiple of `to18ConversionFactor` when `src != gem`.
+    // This constraint is intentionally not enforced in this contract.
     function swapCallback(address src, address /* dst */, uint256 amt, uint256 /* minOut */, address to, bytes calldata /* data */) external auth {
         if (src == gem) PsmLike(psm).sellGemNoFee(to, amt);
         else            PsmLike(psm).buyGemNoFee (to, amt / to18ConversionFactor);
